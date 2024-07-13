f48f1fae97de76f899538c8b8a67e5a2c9ca3ff7
Valters Vingolds <valters@vingolds.ch>
2024-07-13 08:20:49 +0200

    test rewriting cr/lf files

---
+++ source-problems/src/main/java/ch/vingolds/testopenrewrite/WithCrLfLineEndings.java
@@ -0,0 +1,13 @@
+package ch.vingolds.testopenrewrite;
+
+/**
+ * This file is saved with cr/lf line endings
+ * but OpenRewrite rules don't actually fix it?
+ */
+public class WithCrLfLineEndings {
+
+    // this file needs to be rewritten with just \n line endings
+		// this line is indented with tabs and will be rewritten with spaces
+    // but why are line endings not fixed?
+
+}
